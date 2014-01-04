d23521990a0c09aa538de0ddf3d91a90630ac2d0
gingray <gingray.dev@gmail.com>
2014-01-05 00:13:19 +0400

    Add class mapper

---
+++ MacrosProcessor/Core/Mapper/FileNameMapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MacrosProcessor.Common;
+
+namespace MacrosProcessor.Core.Mapper
+{
+    public class FileNameMapper : BaseMapper, IBaseDirectory
+    {
+        private readonly string _filePattern;
+
+        public FileNameMapper(string filePattern, string name)
+            : this(filePattern, name, null)
+        {
+        }
+
+        public FileNameMapper(string filePattern, string name, Func<string, string> filter)
+            : base(name, filter)
+        {
+            _filePattern = filePattern;
+        }
+
+        protected override string Map()
+        {
+            var name = Directory.GetFiles(Path.Combine(BaseDirectory, ""), _filePattern).FirstOrDefault();
+            if (name != null)
+                return Path.GetFileName(name);
+            throw new MapingException(string.Format("FileNameMapper {0} pattern not found",_filePattern));
+        }
+
+        public override object Clone()
+        {
+            return new FileNameMapper(_filePattern, Name, Filter);
+        }
+
+        public string BaseDirectory { get; set; }
+    }
+}
